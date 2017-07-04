9f23364c958b4a0b13b8175b527e7033e404e987
leelilyfutunn <leelily@futunn.com>
2017-07-04 20:04:49 +0800

    update

--- OpenInterface/Python/rqalpha-mod-futu/rqalpha_mod_futu/debug_run_file.py
+++ OpenInterface/Python/rqalpha-mod-futu/rqalpha_mod_futu/debug_run_file.py
@@ -5,16 +5,15 @@ from rqalpha import run_file
 
 config = {
   "base": {
-    "start_date": "2017-06-01",
-    "end_date": "2017-06-20",
-    "securities": ['stock'],
+    "start_date": "2016-01-01",
+    "end_date": "2016-12-01",
     "accounts": {
       "stock": 100000
     },
-    "benchmark": "HK.00002",
+    "benchmark": "HK.00700",
     "frequency": "1d",
     # 运行类型，`b` 为历史数据回测，`p` 为实时数据模拟交易, `r` 为实时数据实盘交易。
-    "run_type":  "p",
+    "run_type":  "b",
   },
   "extra": {
     "log_level": "verbose",
@@ -35,6 +34,10 @@ config = {
   }
 }
 
-strategy_file_path = "./debug_strategy.py"
+# strategy_file_path = "./debug_strategy.py"  # OK
+strategy_file_path = "./test_strategy.py"   # ERROR long_avg
+# strategy_file_path = "./MACD.py"  # OK
+# strategy_file_path = "./RSI.py"   # ERROR 多支股票
+# strategy_file_path = "./Turtles.py"  # ERROE input lengths are different
 
 run_file(strategy_file_path, config)
